3fd54f4bc7d44a75159f360e270d6243b5c88897
Hamza-Noah <hamza.nouh97@gmail.com>
2022-04-13 23:47:11 +0200

    added the editions

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,9 @@
 const menu = document.querySelector(".menu-icon");
 const nav = document.querySelector("nav");
+const sidebar = document.querySelector(".sidebar-content");
 
 menu.addEventListener("click", function(){
     menu.classList.toggle("open");
     nav.classList.toggle("menu-visible");
+    sidebar.classList.toggle("menu-opened");
 });
