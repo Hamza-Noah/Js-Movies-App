44094291bc0adbe745e0d7a39e721fc025fa618a
Hamza-Noah <hamza.nouh97@gmail.com>
2022-04-14 05:22:54 +0200

    added editions to sidebar and started movies seciton

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,20 @@
+// Sidebar
+
 const menu = document.querySelector(".menu-icon");
 const nav = document.querySelector("nav");
 const sidebar = document.querySelector(".sidebar-content");
 
-menu.addEventListener("click", function(){
-    menu.classList.toggle("open");
-    nav.classList.toggle("menu-visible");
-    sidebar.classList.toggle("menu-opened");
+menu.addEventListener("click", function () {
+  menu.classList.toggle("open");
+  nav.classList.toggle("menu-visible");
+  sidebar.classList.toggle("menu-opened");
 });
+
+// Fetching API
+
+async function API() {
+  const API = await fetch(
+    "https://api.themoviedb.org/3/trending/all/day?api_key=f31de6b44ad5fe28935eff41302201c7"
+  );
+  console.log(API.response);
+}
