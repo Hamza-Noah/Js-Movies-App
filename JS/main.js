d38f367120ed85548701bc2107b6e4b7824fe2a0
Hamza-Noah <hamza.nouh97@gmail.com>
2022-04-15 04:57:39 +0200

    added the movies to the applicaiton

--- JS/main.js
+++ JS/main.js
@@ -11,10 +11,44 @@ menu.addEventListener("click", function () {
 });
 
 // Fetching API
+const moviesContainer  = document.querySelector(".movies-container");
 
-async function API() {
-  const API = await fetch(
-    "https://api.themoviedb.org/3/trending/all/day?api_key=f31de6b44ad5fe28935eff41302201c7"
+async function getMovies(type) {
+  let apiRespone = await fetch(
+    `https://api.themoviedb.org/3/${type}?api_key=f31de6b44ad5fe28935eff41302201c7`
   );
-  console.log(API.response);
+  let data = await apiRespone.json();
+  displayMovies(data.results);
 }
+
+function displayMovies(key) {
+  let movies = ``;
+  for (let i = 0; i < key.length; i++) {
+    movies += `
+        <div class="movie">
+        <div class="img">
+             <img class="w-100" src="https://image.tmdb.org/t/p/w500${key[i].poster_path}"/>
+        </div>     
+            <div class=overlay">
+                <h3>${key[i].title}</h3>
+                <p class"description>${key[i].overview}</p>
+                <p class"date">${key[i].release_date}</p>
+                <p class"rate">${key[i].vote_average}</p>
+             </div>
+        </div>
+        `;
+  }
+  moviesContainer.innerHTML = movies;
+}
+
+// getMovies("trending/all/day");
+// getMovies("movie/popular");    
+// getMovies("movie/top_rated");
+// getMovies("movie/upcoming"); 
+getMovies("movie/now_playing"); 
+
+
+
+
+    searchURL = "https://api.themoviedb.org/3/search/movie?query=mad&api_key=f31de6b44ad5fe28935eff41302201c7&language=en-US&include_adult=false";
+   
